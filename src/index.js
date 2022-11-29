1769eb2fdff0f5eb95ea4387de24bf065a4fc316
Jorge Hinojosa <j.hinojosaacevedo@uandresbello.edu>
2022-11-29 03:17:55 -0300

    [FIX] Cors

--- src/index.js
+++ src/index.js
@@ -21,7 +21,12 @@ app.use(bodyParser.json())
 
 mongoose
   .connect(URL_MONGODB, { useNewUrlParser: true })
-  .then(() => {
+  .then((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', FRONT_URL)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
     app.use('/products', products)
     app.use('/auth', auth)
     app.use('/categories', categories)
